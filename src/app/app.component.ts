03cb60ad2766c4de7214faeff4e0712f7c5090e4
Beatriz Oliveira <anabeatrizxoliveira@gmail.com>
2021-01-23 00:32:12 -0300

    add firestore and first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'kanban-fire';
+  todo = this.store.collection<Task>('todo').valueChanges({ idField: 'id' });
+  inProgress = this.store.collection<Task>('inProgress').valueChanges({ idField: 'id' });
+  done = this.store.collection<Task>('done').valueChanges({ idField: 'id' });
+
+  constructor(private dialog: MatDialog, private store: AngularFirestore) {}
+
+  drop(event: CdkDragDrop<string[]>): void {
+    if (event.container.id === event.previousContainer.id) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
+
+  addItem(event: KeyboardEvent): void {
+    const input = event.target as HTMLInputElement;
+    this.todo.push(input.value);
+    input.value = '';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatInputModule } from '@angular/material/input';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { FormsModule } from '@angular/forms';
+import { environment } from 'src/environments/environment';
+import { MatDialogModule } from '@angular/material/dialog';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    DragDropModule,
+    MatInputModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireModule,
+    MatDialogModule
+
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
