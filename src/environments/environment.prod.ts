36ed4026ebd68df7e15eee097df2ddea5bb3bb0b
beatrizoliveiira <anabeatrizxoliveira@gmail.com>
2022-01-08 20:32:54 -0300

    fix(deploy-firebase): fix environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: '...',
+    authDomain: 'project.firebaseapp.com',
+    databaseURL: 'https://project.firebaseio.com',
+    projectId: '1:599447211581:web:863ac0e648fb3f7d7a2454',
+    storageBucket: 'project.appspot.com',
+    messagingSenderId: '...',
+  },
 };
